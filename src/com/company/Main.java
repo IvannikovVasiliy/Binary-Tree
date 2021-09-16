4e9fe86d76009c3a403152b971bbc4121fc1b811
Your Name <you@example.com>
2021-09-16 10:17:15 +0300

    Final version of program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,29 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Tree tree = new Tree();
-        tree.Add("2");
-        tree.Add("*");
-        tree.Add("4");
-        tree.Print();
+        Tree bufTree = new Tree();
+
+        String str = "(3+2*2)*(7+6/3)";
+
+        Boolean addInBufTree = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '(' && str.charAt(i) != ')')
+                if (addInBufTree)
+                    bufTree.Add(new Node(Character.toString(str.charAt(i))));
+                else
+                    tree.Add(new Node(Character.toString(str.charAt(i))));
+            else {
+                if (str.charAt(i) == '(')
+                    addInBufTree = true;
+                if (str.charAt(i) == ')') {
+                    addInBufTree = false;
+                    tree.AddFromBufTree(bufTree.getRoot());
+                    bufTree = new Tree();
+                }
+            }
+        }
+        //tree.Print();
+
+        System.out.println(tree.Result());
     }
 }
--- src/com/company/Tree.java
+++ src/com/company/Tree.java
@@ -1,16 +1,52 @@
 package com.company;
 
 import java.util.HashSet;
+import java.util.Set;
 
 class Node{
-    static HashSet<String> Set(){
+    static HashSet<String> SetPlus() {
+        HashSet<String> setPlus = new HashSet<>();
+        setPlus.add("+");
+        return setPlus;
+    }
+    static HashSet<String> SetMinus() {
+        HashSet<String> setMinus = new HashSet<>();
+        setMinus.add("-");
+        return setMinus;
+    }
+    static  HashSet<String> SetMultiply() {
         HashSet<String> set = new HashSet<>();
-
         set.add("*");
-        set.add("+");
-
         return set;
     }
+    static  HashSet<String> SetDivide() {
+        HashSet<String> set = new HashSet<>();
+        set.add("+");
+        return set;
+    }
+    static HashSet<String> Set(){
+        HashSet<String> set = new HashSet<>();
+        set.add("*");
+        set.add("+");
+        set.add("/");
+        set.add("-");
+        return set;
+    }
+    static HashSet<String> SetPlusMinus(){
+        HashSet<String> set = new HashSet<>();
+        set.add("+");
+        set.add("-");
+        return set;
+    }
+    static HashSet<String> SetMultiplyDivide(){
+        HashSet<String> set = new HashSet<>();
+        set.add("*");
+        set.add("/");
+        return set;
+    }
+
+    private boolean isBrackets = false;
+    public boolean getIsBrackets() { return isBrackets; }
 
     private String Value;
     public String getValue() { return Value; }
@@ -32,23 +68,65 @@ class Node{
         Left = null;
         Right = null;
     }
+    public Node(String value, Node left, Node right){
+        Value = value;
+        Left = left;
+        Right = right;
+    }
 
     public void Add(Node node) {
-        if (node.Value == "(" or )
-
-        if (Value == null) {
-            Value = node.Value;
-            Left = new Node();
-            Right = new Node();
-        }
-        else {
-            if (Set().contains(Value)){
-                Right.Add(new Node(node.Value));
+        if (Set().contains(node.Value)) {
+            if (!Set().contains(Value)) {
+                if (Left == null) {
+                    Left = new Node();
+                    Left.Add(new Node(Value));
+                    Value = node.Value;
+                }
+            } else {
+                if (SetMultiplyDivide().contains(Value) || isBrackets) {
+                    Left = new Node(Value, Left, Right);
+                    Right = null;
+                    Value = node.Value;
+                } else {
+                    if (SetPlusMinus().contains(Value)) {
+                        Right.Add(node);
+                    }
+                }
             }
-            else {
-                Left.Add(new Node(Value));
+        } else {
+            if (Value == null) {
                 Value = node.Value;
+                Left = node.Left;
+                Right = node.Right;
+            } else {
+                if (Left == null) {
+                    Left = new Node();
+                    Left.Add(node);
+                } else {
+                    if (Right == null) {
+                        Right = new Node();
+                        Right.Add(node);
+                    } else {
+                        Right.Add(node);
+                    }
+                }
             }
+        }
+    }
+    public void AddFromBufTree(Node rootBufTree) {
+        if (Value == null) {
+            isBrackets= true;
+            Value = rootBufTree.Value;
+            Left = rootBufTree.Left;
+            Right = rootBufTree.Right;
+        } else {
+            if (Left == null)
+                Left = new Node(rootBufTree.Value, rootBufTree.Left, rootBufTree.Right);
+            else
+                if (Right == null)
+                    Right = new Node(rootBufTree.Value, rootBufTree.Left, rootBufTree.Right);
+                else
+                    Right.Add(rootBufTree);
         }
     }
 
@@ -58,8 +136,30 @@ class Node{
         else
         {
             System.out.println(Value);
-            Left.Print();
-            Right.Print();
+            if (Left != null)
+                Left.Print();
+            if (Right != null)
+                Right.Print();
+        }
+    }
+
+    public int Result() {
+        if (!Set().contains(Value))
+            return Integer.parseInt(Value);
+        else {
+            if (SetPlus().contains(Value))
+                return Left.Result() + Right.Result();
+            else {
+                if (SetMinus().contains(Value))
+                    return Left.Result() - Right.Result();
+                else {
+                    if (SetMultiply().contains(Value))
+                        return Left.Result() * Right.Result();
+                    else {
+                        return Left.Result() / Right.Result();
+                    }
+                }
+            }
         }
     }
 }
@@ -71,11 +171,16 @@ public class Tree{
 
     public Tree() { Root = new Node(); }
 
-    public void Add(String value){
-        Root.Add(new Node(value));
+    public void Add(Node node){
+        Root.Add(node);
+    }
+    public void AddFromBufTree(Node rootBufTree) {
+        Root.AddFromBufTree(rootBufTree);
     }
 
     public void Print(){
         Root.Print();
     }
+
+    public int Result() { return Root.Result(); }
 }
